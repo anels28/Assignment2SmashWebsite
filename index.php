9c6195127ca8c0602df4df37cc053707d1814899
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-09-16 16:19:01 -0400

    initial commit
    Files are added and organized!

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Seattle | Lab 02: Flash Website</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+
+	<body class="container">
+
+		<header>
+			<a href="index.php">
+				<h1 class="homeh1">Team Seattle - Lab 02</h1>
+				<img src="images/seattle.jpg" alt="Seattle">
+			</a>
+		</header>
+
+		<main>
+			<p>Hello World! We are Team Seattle. Jennifer Ngene is our Information Architect, Junting Chen is our Visual Designer, and Jialin Huo is our Technical Coder. Click on the links to learn more about us.</p>
+		</main>
+
+	</body>
+
+<?php include "inc/main-nav.inc"; ?>
+
+<?php include "inc/footer.inc"; ?>
