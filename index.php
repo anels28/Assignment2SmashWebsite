b953a54b9b07bedb92b56d1fe7efd85bf4725c2d
anels28 <55111942+anels28@users.noreply.github.com>
2019-09-18 15:20:56 -0400

    fixing webpage roles

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "inc/main-html-top.inc"; ?>
 <?php include "inc/main-nav.inc"; ?>
 		<main>
-			<p> Check out our Smash Website. Islomzhan is our Information Architect, Alyssa is our Visual Designer, & Kat is our Technical Coder.
+			<p> Check Out Our Smash Website. Islomzhan is our Information Architect, Alyssa is our Technical Coder, & Kat is our Visual Designer.
 			Thanks to the Team Seattle for their web content!!!</p>
 		</main>
 <?php include "inc/footer.inc"; ?>
