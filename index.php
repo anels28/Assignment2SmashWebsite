4c4efb8980efb8502d16c44404e1595f623cf82e
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-09-23 15:16:41 -0400

    fixed html nav

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
-<?php include "inc/main-html-top.inc"; ?>
-<?php include "inc/main-nav.inc"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title> Team Berlin | Assignment 02: Smash Website </title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+
+	<body class="container">
+
+		<header>
+			<a href="index.php">
+				<h1 class="homeh1" style = "font-size: 1.2em; left: -65px; font-size: 42px;" > Welcome to our page!</h1>
+				<img src="images/berlin.jpg" alt="Seattle">
+			</a>
+		</header>
+
+		<aside>
+			<ul>
+				<li>Hello World.</li>
+				<li>This is the Second Assignment.</li>
+				<li>We are Team Berlin.</li>
+			</ul>
+		</aside>
+        
+        <nav class="menu">
+            <ul>
+                <li><a class="is-current" href="index.php">Home</a></li>
+                <li><a href="jialin.php">Jialin Huo</a></li>
+                <li><a href="junting.php">Junting Chen</a></li>
+                <li><a href="jennifer.php">Jennifer Ngene</a></li>
+            </ul>
+        </nav>
+
 		<main>
 			<p> Check Out Our Smash Website. Islomzhan is our Information Architect, Alyssa is our Technical Coder, & Kat is our Visual Designer.
 			Thanks to the Team Seattle for their web content!!!</p>
 		</main>
+        
 <?php include "inc/footer.inc"; ?>
